e4d25b702c1ddc3a82166158cdbe33f2a8f6e121
Gabi <66589504+xmishix@users.noreply.github.com>
2024-06-28 13:37:51 -0500

    Create prompt-1.py

---
+++ prompt-1.py
@@ -0,0 +1,14 @@
+prompt = """The movie was awesome. > Positive
+I hate icecream. > Negative
+I love flowers. > Positive
+My favorite season is Fall. > Positive
+This food is terrible. > Negative
+I hate insects. >
+"""
+
+messages = [
+    {
+        "role" : "user: ",
+        "content" : prompt
+    }
+]
