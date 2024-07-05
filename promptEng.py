5b0f2d23c6168a5a1ac5701ee75179236d33629c
Gabi <66589504+xmishix@users.noreply.github.com>
2024-07-05 15:09:37 -0500

    Update promptEng.py

--- promptEng.py
+++ promptEng.py
@@ -1,8 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
-model_id = "google/gemma-2b-it"                                 # Identifier of pre-trained model
-tokenizer = AutoTokenizer.from_pretrained(model_id)             # Loads tokenizer associated with the model. The tokenizer is responsible for converting input text into a format that the model can understand.
-model = AutoModelForCausalLM.from_pretrained(model_id)          # Loads the actual model. 
+model_id = "google/gemma-2b-it"                     # Identifier of pre-trained model
+dtype = torch.bfloat16                              # half-precision floating-point format that uses less memory
+
+tokenizer = AutoTokenizer.from_pretrained(model_id) # Loads tokenizer associated with the model. The tokenizer is responsible for converting input text into a format that the model can understand.
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    device_map = "auto",
+    torch_dtype = dtype
+)   # Loads the actual model. It specifies the model ID, sets the device to auto (GPU), and defines the tensor data type
 
 # Function to generate the response
 def generate_response(prompt):
@@ -11,8 +18,7 @@ def generate_response(prompt):
     response = tokenizer.decode(outputs[0][len(inputs[0]):], skip_special_tokens=True)
     return response
 
-
-# Basic example
+# Basic example for prompt engineering
 prompt = """The odd numbers in this group add up to an even number: 4, 8, 9, 15, 12, 2, 1.
 A: The answer is False.
 
@@ -28,6 +34,7 @@ A: The answer is False.
 The odd numbers in this group add up to an even number: 15, 32, 5, 13, 82, 7, 1. 
 A:"""
 
+
 messages = [
     {
         "role": "user",
@@ -35,7 +42,11 @@ messages = [
     }
 ]
 
+# This method returns the prepared prompt ready for text generation. Prompt prepares the prompt for generating text. 
+# Tokenize = false indicates that the tokenizer should not tokenize the input. add_generation_prompt = True acutomatically prepend a generation prompt to the input text.
+text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
 # call the generate_response()
-response = generate_response(messages)
+response = generate_response(text)
 # Display the response
 print(response)
